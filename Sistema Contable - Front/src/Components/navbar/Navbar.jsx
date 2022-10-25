ef7b23857318a9d2db02da6990b7b108fd49f139
Emadlgg <115298783+Emadlgg@users.noreply.github.com>
2022-10-25 16:22:29 -0600

    Archivos

---
+++ Sistema Contable - Front/src/Components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="nav">
+      <a href="#" className="nav__brand">
+        Pagina Principal
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
