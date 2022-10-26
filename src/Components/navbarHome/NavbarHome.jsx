d3914219bd8d1b607e005d2314c7a8dc7bcf4c6d
Emadlgg <osmanemanuel2004@gmail.com>
2022-10-26 17:08:09 -0600

    archivos

---
+++ src/Components/navbarHome/NavbarHome.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./navbarHome.css";
+
+function NavbarHome() {
+
+  return (
+    <nav className="nav">
+      <a href="Login" className="nav__brand">
+        Iniciar Sesiòn
+      </a>
+    </nav>
+  );
+}
+
+export default NavbarHome;
